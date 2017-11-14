7206ebd97da5cb65117ab66dbde7e96107268ae7
Michael Pope <popemray1986@gmail.com>
2017-11-14 00:34:10 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,18 +1,29 @@
 class Song
-  
+
   attr_accessor :name, :artist :genre
 
   @@count = 0
 
   def initialize
     @@count +=1
+    @@genres
+    @@artists
   end
 
   def self.count
     @@count
   end
 
-  
-  
+  @@genres = []
+
+  def self.genres
+    @@genres
+  end
+
+  @@artists = []
+
+  def self.artists
+    @@artists
+  end
 
 end
